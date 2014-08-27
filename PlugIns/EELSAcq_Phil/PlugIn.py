2420ad60c1231f7cfc134b9e7b8a75da6e40b801
Chris Meyer <cmeyer1969@gmail.com>
2014-08-27 21:35:42 +0000

    Fix problem of missing 'find image panel' method.
    Was svn r2279

--- PlugIns/EELSAcq_Phil/PlugIn.py
+++ PlugIns/EELSAcq_Phil/PlugIn.py
@@ -133,7 +133,14 @@ class AcquireController(object):
         def show_in_panel(data_item, document_controller, image_panel_id):
             workspace = document_controller.workspace
             document_controller.document_model.append_data_item(data_item)
-            workspace.get_image_panel_by_id(image_panel_id).set_displayed_data_item(data_item)
+            # argh
+            found_image_panel = None
+            for image_panel in workspace.image_panels:
+                if image_panel.element_id == image_panel_id:
+                    found_image_panel = image_panel
+                    break
+            if found_image_panel:
+                found_image_panel.set_displayed_data_item(data_item)
 
         def add_line_profile(data_item, document_controller, image_panel_id, midpoint=0.5, integration_width=.25):
             document_model = document_controller.document_model
