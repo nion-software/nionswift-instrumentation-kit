c2889a027b7e2920822fa3b96948bf0d06a10e9a
Chris Meyer <cmeyer1969@gmail.com>
2017-08-11 20:07:47 -0700

    Add license and setup file.

---
+++ setup.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+"""
+To upload to PyPI, PyPI test, or a local server:
+python setup.py bdist_wheel upload -r <server_identifier>
+"""
+
+import setuptools
+import os
+
+setuptools.setup(
+    name="nionswift-instrumentation-kit",
+    version="0.0.1",
+    author="Nion Software",
+    author_email="swift@nion.com",
+    description="Library and UI kit for STEM instrumentation (Camera, Scan, Microscope) with Nion Swift.",
+    url="https://github.com/nion-software/nionswift-instrumentation-kit",
+    packages=["nion.instrumentation", "nionswift_plugin.nion_instrumentation_ui"],
+    license='GPLv3',
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Programming Language :: Python :: 3.5",
+    ],
+    include_package_data=True,
+    test_suite="nion.instrumentation.test",
+    python_requires='~=3.5',
+)
