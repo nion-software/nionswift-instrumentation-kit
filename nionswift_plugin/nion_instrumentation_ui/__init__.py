3d55a1b0326cbd683c7c133c2ce8198ca9fc9c76
Chris Meyer <cmeyer1969@gmail.com>
2017-08-13 16:38:44 -0700

    Ensure camera base is initialized during plug-in loading.

--- nionswift_plugin/nion_instrumentation_ui/__init__.py
+++ nionswift_plugin/nion_instrumentation_ui/__init__.py
@@ -2,6 +2,9 @@ from . import CameraControlPanel
 from . import ScanControlPanel
 from . import ScanAcquisition
 
+from nion.instrumentation import camera_base
+
 def run():
+    camera_base.run()
     CameraControlPanel.run()
     ScanControlPanel.run()
