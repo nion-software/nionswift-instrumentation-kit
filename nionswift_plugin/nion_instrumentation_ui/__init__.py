ecc0ce85864e73ed820920589101e69508aa7094
Chris Meyer <cmeyer1969@gmail.com>
2017-08-15 09:01:19 -0700

    Fix ProbeViewController initialization (now when plug-in loads).

--- nionswift_plugin/nion_instrumentation_ui/__init__.py
+++ nionswift_plugin/nion_instrumentation_ui/__init__.py
@@ -3,6 +3,23 @@ from . import ScanControlPanel
 from . import ScanAcquisition
 
 from nion.instrumentation import camera_base
+from nion.instrumentation import stem_controller
+
+
+class STEMControllerExtension:
+
+    # required for Swift to recognize this as an extension class.
+    extension_id = "nion.stem_controller"
+
+    def __init__(self, api_broker):
+        # grab the api object.
+        api = api_broker.get_api(version="1", ui_version="1")
+        self.__probe_view_controller = stem_controller.ProbeViewController(api.application._application.event_loop)
+
+    def close(self):
+        self.__probe_view_controller.close()
+        self.__probe_view_controller = None
+
 
 def run():
     camera_base.run()
