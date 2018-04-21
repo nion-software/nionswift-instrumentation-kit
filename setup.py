092293b765fa23706e09ca184fc3b290a5e0cab5
Chris Meyer <cmeyer1969@gmail.com>
2018-04-21 12:34:59 -0700

    Clean up setup.py files in preparation for PyPI.

--- setup.py
+++ setup.py
@@ -10,13 +10,14 @@ import os
 
 setuptools.setup(
     name="nionswift-instrumentation-kit",
-    version="0.0.1",
+    version="0.1.0",
     author="Nion Software",
     author_email="swift@nion.com",
     description="Library and UI kit for STEM instrumentation (Camera, Scan, Microscope) with Nion Swift.",
     url="https://github.com/nion-software/nionswift-instrumentation-kit",
-    packages=["nion.instrumentation", "nionswift_plugin.nion_instrumentation_ui"],
-    install_requires=['nionswift'],
+    packages=["nion.instrumentation", "nion.instrumentation.test", "nionswift_plugin.nion_instrumentation_ui"],
+    package_data={"nionswift_plugin.nion_instrumentation_ui": ["resources/*"]},
+    install_requires=[],
     license='GPLv3',
     classifiers=[
         "Development Status :: 2 - Pre-Alpha",
