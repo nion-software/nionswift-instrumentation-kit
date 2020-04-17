c5f7ce571cfdbe585630b84ea135c946d42d99fa
Chris Meyer <cmeyer1969@gmail.com>
2020-04-16 18:14:02 -0700

    Use graphic set tools for probe graphic.

--- nion/instrumentation/stem_controller.py
+++ nion/instrumentation/stem_controller.py
@@ -1,7 +1,6 @@
 # standard libraries
 import abc
 import asyncio
-import copy
 import enum
 import functools
 import gettext
@@ -15,7 +14,6 @@ import typing
 # local libraries
 from nion.swift.model import Graphics
 from nion.swift.model import HardwareSource
-from nion.utils import Binding
 from nion.utils import Event
 from nion.utils import Geometry
 from nion.utils import Model
@@ -552,196 +550,67 @@ def ScanContextDisplayItemListModel(document_model: "DocumentModel.DocumentModel
     return DisplayItemListModel(document_model, "display_items", is_scan_context_display_item, stem_controller.scan_context_data_item_changed_event)
 
 
-class PropertyToGraphicBinding(Binding.PropertyBinding):
-
-    """
-        Binds a property of an operation item to a property of a graphic item.
-    """
-
-    def __init__(self, event_loop, region, region_property_name, graphic, graphic_property_name):
-        super().__init__(region, region_property_name)
-        self.__graphic = graphic
-        self.__graphic_property_changed_listener = graphic.property_changed_event.listen(self.__property_changed)
-        self.__graphic_property_name = graphic_property_name
-        self.__region_property_name = region_property_name
-        self.__task = None
-        def set_target_value(value):
-            async def do_set_value():
-                if value is not None:
-                    setattr(self.__graphic, graphic_property_name, value)
-                self.__task = None
-            if self.__task:
-                self.__task.cancel()
-            self.__task = event_loop.create_task(do_set_value())
-        self.target_setter = set_target_value
-
-    def close(self):
-        if self.__task:
-            self.__task.cancel()
-            self.__task = None
-        self.__graphic_property_changed_listener.close()
-        self.__graphic_property_changed_listener = None
-        self.__graphic = None
-        super().close()
-
-    # watch for property changes on the graphic.
-    def __property_changed(self, property_name):
-        if property_name == self.__graphic_property_name:
-            old_property_value = getattr(self.source, self.__region_property_name)
-            # to prevent message loops, check to make sure it changed
-            property_value = getattr(self.__graphic, property_name)
-            if property_value is not None:
-                property_value = Geometry.FloatPoint(y=property_value[0], x=property_value[1])
-            if property_value != old_property_value:
-                self.update_source(property_value)
-
-
-class ProbeGraphicConnection:
-    """Manage the connection between the hardware and the graphics representing the probe on a display."""
-
-    def __init__(self, event_loop, display_item, probe_position_value, hide_probe_graphics_fn):
-        self.event_loop = event_loop
-        self.display_item = display_item
-        self.probe_position_value = probe_position_value
-        self.graphic = None
-        self.binding = None
-        self.remove_region_graphic_event_listener = None
-        self.hide_probe_graphics_fn = hide_probe_graphics_fn
-
-    def close(self):
-        graphic = self.graphic
-        self.hide_probe_graphic()
-        if graphic:
-            self.display_item.remove_graphic(graphic)
-
-    def update_probe_state(self, probe_position):
-        if probe_position is not None:
-            self.probe_position_value.value = probe_position
-            self.show_probe_graphic()
-        else:
-            graphic = self.graphic
-            self.hide_probe_graphic()
-            if graphic:
-                self.display_item.remove_graphic(graphic)
-        if self.graphic:
-            self.graphic.color = "#F80"
-
-    def show_probe_graphic(self):
-        if not self.graphic:
-            self.graphic = Graphics.PointGraphic()
-            self.graphic.graphic_id = "probe"
-            self.graphic.label = _("Probe")
-            self.graphic.position = self.probe_position_value.value
-            self.graphic.is_bounds_constrained = True
-            self.display_item.add_graphic(self.graphic)
-            self.binding = PropertyToGraphicBinding(self.event_loop, self.probe_position_value, "value", self.graphic, "position")
-            def graphic_removed():
-                self.hide_probe_graphic()
-                # next make sure all other probe graphics get hidden so that setting the probe_position_value
-                # doesn't set graphics positions to None
-                self.hide_probe_graphics_fn()
-                self.probe_position_value.value = None
-            def display_removed():
-                self.hide_probe_graphic()
-            self.remove_region_graphic_event_listener = self.graphic.about_to_be_removed_event.listen(graphic_removed)
-            self.display_about_to_be_removed_listener = self.display_item.about_to_be_removed_event.listen(display_removed)
-
-    def hide_probe_graphic(self):
-        if self.graphic:
-            self.binding.close()
-            self.binding = None
-            self.remove_region_graphic_event_listener.close()
-            self.remove_region_graphic_event_listener = None
-            self.display_about_to_be_removed_listener.close()
-            self.display_about_to_be_removed_listener = None
-            self.graphic = None
-
-
-class ProbeView:
+class ProbeView(AbstractGraphicSetHandler):
     """Observes the probe (STEM controller) and updates data items and graphics."""
 
     def __init__(self, stem_controller: STEMController, document_model, event_loop: asyncio.AbstractEventLoop):
-        assert event_loop is not None
-        self.__document_model = document_model
+        self.__stem_controller = stem_controller
         self.__event_loop = event_loop
-        self.__last_data_items_lock = threading.RLock()
-        self.__last_data_items = list()
+        self.__scan_display_items_model = ScanContextDisplayItemListModel(document_model, stem_controller)
+        self.__graphic_set = GraphicSetController(self)
+        # note: these property changed listeners can all possibly be fired from a thread.
         self.__probe_state = None
-        self.__probe_graphic_connections = list()
         self.__probe_position_value = stem_controller._probe_position_value
         self.__probe_state_changed_listener = stem_controller.probe_state_changed_event.listen(self.__probe_state_changed)
-        self.__probe_data_items_changed_listener = stem_controller.scan_data_item_states_changed_event.listen(self.__probe_data_item_states_changed)
 
     def close(self):
-        self.__probe_data_items_changed_listener.close()
-        self.__probe_data_items_changed_listener = None
         self.__probe_state_changed_listener.close()
         self.__probe_state_changed_listener = None
-        self.__last_data_items = list()
+        self.__graphic_set.close()
+        self.__graphic_set = None
+        self.__scan_display_items_model.close()
+        self.__scan_display_items_model = None
         self.__event_loop = None
-
-    def __probe_data_item_states_changed(self, data_item_states):
-        # thread safe.
-        if not any(data_item_state.get("channel_id").endswith("_subscan") for data_item_state in data_item_states):
-            with self.__last_data_items_lock:
-                self.__last_data_items = [data_item_state.get("data_item") for data_item_state in data_item_states]
+        self.__stem_controller = None
 
     def __probe_state_changed(self, probe_state, probe_position):
         # thread safe. move actual call to main thread using the event loop.
-        self.__latest_probe_state = probe_state
-        self.__latest_probe_position = probe_position
-        self.__event_loop.call_soon_threadsafe(self.__update_probe_state)
+        self.__event_loop.call_soon_threadsafe(self.__update_probe_state, probe_state, probe_position)
 
-    def __update_probe_state(self) -> None:
-        # thread unsafe. always called on main thread (via event loop).
-        # don't pass arguments to this function; instead use 'latest' values.
-        # this helps avoid strange cyclic updates.
+    def __update_probe_state(self, probe_state, probe_position) -> None:
         assert threading.current_thread() == threading.main_thread()
-        probe_state = self.__latest_probe_state
-        probe_position = self.__latest_probe_position
-        if probe_state != self.__probe_state:
-            if probe_state == "scanning":
-                self.__hide_probe_graphics()
-            else:
-                self.__show_probe_graphics(probe_position)
-            self.__probe_state = probe_state
-        self.__update_probe_graphics(probe_position)
+        if probe_state != "scanning" and probe_position is not None:
+            self.__graphic_set.synchronize_graphics(self.__scan_display_items_model.display_items)
+        else:
+            self.__graphic_set.remove_all_graphics()
 
-    def __hide_probe_graphics(self):
-        # thread unsafe.
-        probe_graphic_connections = copy.copy(self.__probe_graphic_connections)
-        self.__probe_graphic_connections = list()
-        for probe_graphic_connection in probe_graphic_connections:
-            probe_graphic_connection.close()
+    # implement methods for the graphic set handler
 
-    def __show_probe_graphics(self, probe_position):
-        # thread unsafe.
-        with self.__last_data_items_lock:
-            data_items = self.__last_data_items
-            # self.__last_data_items = list()
-        for data_item in data_items:
-            # scanning has stopped, figure out the displays that might be used to display the probe position
-            # then watch for changes to that list. changes will include the display being removed by the user
-            # or newer more appropriate displays becoming available.
-            display_item = self.__document_model.get_display_item_for_data_item(data_item)
-            if display_item:
-                # the probe position value object gives the ProbeGraphicConnection the ability to
-                # get, set, and watch for changes to the probe position.
-                probe_graphic_connection = ProbeGraphicConnection(self.__event_loop, display_item, self.__probe_position_value, self.__hide_probe_graphics)
-                probe_graphic_connection.update_probe_state(probe_position)
-                self.__probe_graphic_connections.append(probe_graphic_connection)
+    def _graphic_removed(self, subscan_graphic: Graphics.Graphic) -> None:
+        # clear probe state
+        self.__probe_position_value.value = None
 
-    def __update_probe_graphics(self, probe_position):
-        # thread unsafe.
-        for probe_graphic_connection in self.__probe_graphic_connections:
-            probe_graphic_connection.update_probe_state(probe_position)
+    def _create_graphic(self) -> Graphics.PointGraphic:
+        graphic = Graphics.PointGraphic()
+        graphic.graphic_id = "probe"
+        graphic.label = _("Probe")
+        graphic.position = self.__probe_position_value.value
+        graphic.is_bounds_constrained = True
+        graphic.color = "#F80"
+        return graphic
+
+    def _update_graphic(self, graphic: Graphics.Graphic) -> None:
+        graphic.position = self.__probe_position_value.value
+
+    def _graphic_property_changed(self, graphic: Graphics.Graphic, name: str) -> None:
+        if name == "position":
+            self.__probe_position_value.value = graphic.position
 
 
 class SubscanView(AbstractGraphicSetHandler):
     """Observes the STEM controller and updates data items and graphics."""
 
     def __init__(self, stem_controller: STEMController, document_model: "DocumentModel.DocumentModel", event_loop: asyncio.AbstractEventLoop):
-        assert event_loop is not None
         self.__stem_controller = stem_controller
         self.__event_loop = event_loop
         self.__scan_display_items_model = ScanContextDisplayItemListModel(document_model, stem_controller)
