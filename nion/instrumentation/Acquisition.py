291334013e7556ae910b8b8cabe1c1385cd6e27b
Chris Meyer <cmeyer1969@gmail.com>
2021-11-09 17:20:49 -0800

    Fix expand_shape to return an int rather than numpy.int64.

--- nion/instrumentation/Acquisition.py
+++ nion/instrumentation/Acquisition.py
@@ -182,7 +182,7 @@ def get_slice_shape(slices: SliceType, shape: ShapeType) -> ShapeType:
 
 
 def expand_shape(shape: ShapeType) -> int:
-    return numpy.product(shape, dtype=numpy.int64)  # type: ignore
+    return int(numpy.product(shape, dtype=numpy.int64))  # type: ignore
 
 
 def get_slice_rect(slices: SliceType, shape: ShapeType) -> Geometry.IntRect:
