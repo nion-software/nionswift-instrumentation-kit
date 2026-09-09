110b6e78e63859517f76b1cafb3cc5e1c9ede81c
Kevin Langley <kevin.langley@bruker.com>
2026-09-09 10:02:00 +0100

    Add classes to reserve and release resources

--- nion/instrumentation/stem_controller.py
+++ nion/instrumentation/stem_controller.py
@@ -14,6 +14,7 @@ import threading
 import time
 import typing
 import uuid
+import weakref
 
 # third party libraries
 # None
@@ -305,6 +306,7 @@ class ScanSpecifier:
 
 
 _TryValueType = typing.TypeVar('_TryValueType', covariant=True)
+_ReservedType = typing.TypeVar('_ReservedType')
 
 class TryValue(typing.Generic[_TryValueType]):
     """ A value and an exception.
@@ -332,6 +334,104 @@ class TryValue(typing.Generic[_TryValueType]):
     @property
     def is_valid(self) -> bool:
         return self.exception is None
+
+
+class InvalidThreadError(RuntimeError):
+    """Raised when a method is called from a thread other than the main thread."""
+    pass
+
+
+class ReservedResource(typing.Generic[_ReservedType]):
+    """A main-thread reservation token for an acquired resource.
+
+    Threading contract:
+    - ``reserve`` and ``release`` occur on the main thread.
+    - Do not pass this token to worker threads.
+    - If worker code needs access, pass only ``resource``.
+    """
+
+    def __init__(self, resource: _ReservedType, release_fn: typing.Callable[[], None]) -> None:
+        self.resource: _ReservedType | None = resource
+        self.__release_fn: typing.Callable[[], None] | None = release_fn
+
+    def __enter__(self) -> ReservedResource[_ReservedType]:
+        return self
+
+    def __exit__(self, exc_type: typing.Any, exc_val: typing.Any, exc_tb: typing.Any) -> None:
+        self.release()
+
+    def release(self) -> None:
+        """Release this reservation token.
+
+        Must be called on the main thread.
+        """
+        if threading.current_thread() is not threading.main_thread():
+            raise InvalidThreadError("release must be called on the main thread.")
+        if self.__release_fn is not None:
+            self.__release_fn()
+            self.__release_fn = None
+        self.resource = None
+
+
+class ResourceReservation(typing.Generic[_ReservedType]):
+    """Main-thread reservation manager for one optional resource."""
+
+    def __init__(self, resource_provider: typing.Callable[[], _ReservedType | None]) -> None:
+        self.__resource_provider = resource_provider
+        self.__reservation: weakref.ReferenceType[ReservedResource[_ReservedType]] | None = None
+        self.__is_available_stream = Stream.ValueStream[bool](False)
+        self.__update_availability_stream()
+
+    @property
+    def is_available_stream(self) -> Stream.AbstractStream[bool]:
+        """A stream of availability of the resource for UI binding."""
+        return self.__is_available_stream
+
+    @property
+    def is_reserved(self) -> bool:
+        reservation_ref = self.__reservation
+        if reservation_ref is None:
+            return False
+        reservation = reservation_ref()
+        if reservation is None:
+            self.__reservation = None
+            return False
+        return True
+
+    def reserve(self) -> ReservedResource[_ReservedType] | None:
+        """Reserve and return a token, or None if unavailable or already reserved.
+
+        Must be called on the main thread.
+        """
+        if threading.current_thread() is not threading.main_thread():
+            raise InvalidThreadError("reserve must be called on the main thread.")
+        if self.is_reserved:
+            return None
+        resource = self.__resource_provider()
+        if resource is None:
+            self.__update_availability_stream()
+            return None
+        reservation = ReservedResource(resource, self.release)
+        self.__reservation = weakref.ref(reservation)
+        self.__update_availability_stream()
+        return reservation
+
+    def release(self) -> None:
+        """Release the current reservation.
+
+        Must be called on the main thread.
+        """
+        if threading.current_thread() is not threading.main_thread():
+            raise InvalidThreadError("release must be called on the main thread.")
+        self.__reservation = None
+        self.__update_availability_stream()
+
+    def notify_resource_state_changed(self) -> None:
+        """Update availability stream when underlying resource state may have changed."""
+        self.__update_availability_stream()
+
+    def __update_availability_stream(self) -> None:
+        self.__is_available_stream.value = self.__resource_provider() is not None and not self.is_reserved
 
 
 class STEMController(Observable.Observable):
